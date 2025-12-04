c905fdf2623b6baf2b81495b3439e108608b9a59
Watashicuvu <106665121+Watashicuvu@users.noreply.github.com>
2025-12-04 09:17:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 from pydantic import SecretStr
 from pathlib import Path
 
+# load dot_env can be placed there
 key = os.getenv('API_KEY', '')
-base_url = 'http://192.168.1.127:1235/v1'
-model = 'gemma-3-12b-it'
-#model = 'qwen3-vl-8b-instruct-mlx'
-#fallback_template_path = Path('world_output') / 'snapshots' / 'world_epoch_0.json'
+base_url = os.getenv('BASE_URL', 'http://192.168.1.127:1235/v1')
+model = os.getenv('MODEL', 'qwen3-vl-8b-instruct-mlx')
+
 fallback_template_path = Path('world_output') / 'world_final.json'
 
 api_key = SecretStr(secret_value=key)
